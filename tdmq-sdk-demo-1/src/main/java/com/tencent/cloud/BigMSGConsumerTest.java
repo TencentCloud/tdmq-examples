3c030b66da636efbb8ce6ce90ce9516903308d85
jshuangtao <jshuangtao@tecent.com>
2023-05-08 15:40:42 +0800

    add big message compress test

---
+++ tdmq-sdk-demo-1/src/main/java/com/tencent/cloud/BigMSGConsumerTest.java
@@ -0,0 +1,78 @@
+package com.tencent.cloud;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.apache.pulsar.client.api.AuthenticationFactory;
+import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.SubscriptionInitialPosition;
+import org.apache.pulsar.client.api.SubscriptionType;
+import org.apache.pulsar.shade.com.google.common.base.Strings;
+
+public class BigMSGConsumerTest {
+
+    public static void main(String[] args) throws Exception {
+
+        LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+        Configuration config = ctx.getConfiguration();
+        LoggerConfig loggerConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
+        loggerConfig.setLevel(Level.INFO);
+        ctx.updateLoggers();
+
+        System.out.printf(" args: %s \n",args);
+        //接入地址
+        String SERVICE_URL = args[0];
+        //token
+        String PROD_TOKEN = args[1];
+        //topic名称
+        String TOPIC_NAME = args[2];
+        //订阅名称
+        String SUB_NAME = args[3];
+        //消息条数
+        int msgCount = Integer.valueOf(args[4]);
+        //间隔
+        int sleepms = Integer.valueOf(args[5]);
+        //tag
+        String tag = args[6];
+
+        PulsarClient pulsarClient = PulsarClient.builder()
+                .serviceUrl(SERVICE_URL)//完整复制控制台接入点地址
+                .authentication(AuthenticationFactory.token(PROD_TOKEN))
+                .statsInterval(5, TimeUnit.SECONDS)
+                .build();
+
+        System.out.println("=================================Start consume msg=====================================");
+        // 订阅相关参数，可用来设置订阅标签(TAG)
+        HashMap<String, String> subProperties = new HashMap<>();
+        if (!Strings.isNullOrEmpty(tag) && !tag.equals("null")){
+            subProperties.put(tag,"1");
+        }
+        Consumer<byte[]> consumer = pulsarClient.newConsumer()
+                .topic(TOPIC_NAME)
+                .subscriptionName(SUB_NAME)
+                .subscriptionType(SubscriptionType.Shared)
+                .autoUpdatePartitionsInterval(1000,TimeUnit.MILLISECONDS)
+                .subscriptionInitialPosition(SubscriptionInitialPosition.Earliest)
+                .ackTimeout(1000,TimeUnit.MILLISECONDS)
+                .subscriptionProperties(subProperties)
+                .subscribe();
+
+        for(int j=0; j< msgCount; j++){
+            TimeUnit.MILLISECONDS.sleep(sleepms);
+            Message msg = consumer.receive();
+            System.out.println("消息大小为:"+msg.getData().length/1024/1024+"MB");
+            System.out.printf("%s 消费完成 第%d条 msgid:%s tag:%s key:%s sequenceID:%d\n ", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")),j+1,msg.getMessageId(),msg.getProperties(),msg.getKey(),msg.getSequenceId());
+            consumer.acknowledge(msg);
+            System.out.printf("%s ask完成 第%d条 msgid:%s \n ",LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")),j+1,msg.getMessageId());
+        }
+    }
+}
---
+++ tdmq-sdk-demo-1/src/main/java/com/tencent/cloud/BigMSGProducerTest.java
@@ -0,0 +1,97 @@
+package com.tencent.cloud;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import org.apache.pulsar.client.api.*;
+import java.util.concurrent.TimeUnit;
+
+public class BigMSGProducerTest {
+
+    public static void main(String[] args) throws Exception {
+
+        Map<Integer,CompressionType> zipTypeMap = new HashMap<>();
+        zipTypeMap.put(1,CompressionType.LZ4);
+        zipTypeMap.put(2,CompressionType.ZLIB);
+        zipTypeMap.put(3,CompressionType.ZSTD);
+        zipTypeMap.put(4,CompressionType.SNAPPY);
+
+        System.out.printf("args: %s \n",args);
+        //接入地址
+        String SERVICE_URL = args[0];
+        //token
+        String PROD_TOKEN = args[1];
+        //topic名称
+        String TOPIC_NAME = args[2];
+        //订阅名称
+//        String SUB_NAME = args[3];
+        //生产条数
+        int msgCount = Integer.valueOf(args[4]);
+        //生产间隔
+        int sleepms = Integer.valueOf(args[5]);
+        // 是否开始batch(是否关闭chunk)
+        int chunk = Integer.valueOf(args[6]);
+        // 是否压缩
+        int compre = Integer.valueOf(args[7]);
+        // 消息大小 单位为k
+        int msgSize = Integer.valueOf(args[8]);
+        //压缩方式
+        int zipType = Integer.valueOf(args[9]);
+        //消息内容
+        int messageContentType = Integer.valueOf(args[10]);
+
+        PulsarClient pulsarClient = PulsarClient.builder()
+                .serviceUrl(SERVICE_URL)//完整复制控制台接入点地址
+                .authentication(AuthenticationFactory.token(PROD_TOKEN)).statsInterval(5, TimeUnit.SECONDS)
+                .build();
+        ProducerBuilder pbu =  pulsarClient.newProducer();
+        if (chunk > 0){
+            pbu.enableBatching(false);
+            pbu.enableChunking(true);
+        }else{
+            pbu.enableBatching(true);
+            pbu.enableChunking(false);
+        }
+        if (compre > 0){
+            pbu.compressionType(zipTypeMap.get(zipType));
+        }else{
+            pbu.compressionType(CompressionType.NONE);
+        }
+        Producer<byte[]> producer = pbu.topic(TOPIC_NAME).create();
+        String raw = "ST6H6sW5zYBRalFbVQF0QicoSdNctAQ1zjYgqQfmzaRwYluPLf5r4k8pxZbA5lDOPn3NdnYnvc5dE2dtoO7F51bFrjJgbHsIQfVERgKIddNQq7Dw5RAAS6UvtDB2JVtJcDDwEzrLTRnra16pEpYYlcNMuaezhvtBg2tngGI8Zu8BXxYpDZxHYmainDHCz2jNnzYGa2GPBbvtUEITvdBkmM9Ct9V4xge2uYL4btd1SVwlJ8bduJmV1r22njv3QmjaxCXhT2gwK5lGMq7Xf00426dIMSiNFIltJlINl2ezPVKIRYkGDD63A45phwDOIuKdO4JoFMzd5ps9YDVbXBoNKVG5RhK8O7oPiAFaESL8bkSCU0OIUN8bXqMZXXoMA7fUKlNPQpb3cwOKv9H159vRGr91vxSk7YZF4brh2O7bHnp5IXJzgOUMaBhwHrkhYSlVoo5bHTQx9K10kLAJALiLMP75XDKFzCrwLOB04UCsr3DcjUiY8ASrCEiDaUkOSnuJ6MTO1XKNZaap9zYuxCDioqemMHYzomGOI2cy3DRu34bI3SeyT30tlEJ1klGeujtCu7V1v4WlT6FnzGZi8na3TfGR4RQVfOoBo01V8IndTYFkxcXWbtZe8aw1EJVK2emOs97jQaMMJnoegSUrvx8AVXNrQvnipOgzxANewh4vuR24qgHeL9UAvITaTEDBhKoeoKsheQh5qR6v24PnvA1W4zG2hmLPmCdSCejOl3q3RrqVQ13hwAxqT1JEX9OASHal7GslaK6ajPMqNthYDxIJ1wn5oeVHwHSQThXWWnhoJfZwFCv2OneLbGlamXZAI8KmW2f2qAIfynKB3Tc6K29AO6iqadf1pJR1axb3fajKoUBfu9ZOK0DswaU8Aag5GgI7O33taei2Vdf8peCcmnORmprDrV7TvKg2Q0KNTleYnNxN0DF84eSS4yLBVCYUp1t6p0BQz2U1EYdPjVniBZf5942sZvhAUiLwJgnH8diimkzAnV72QMF7ChIelJ7FEwcf";
+        String randomMessage = null;
+        if (messageContentType == 1){
+            Random random = new Random();
+            String str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*¥#@!$%^&*";
+            StringBuffer sb=new StringBuffer();
+            for(int i=0;i<1024;i++){
+                int number=random.nextInt(62);
+                sb.append(str.charAt(number));
+            }
+            randomMessage = sb.toString();
+        }
+
+        StringBuilder sb = new StringBuilder(msgSize*1024);
+        for(int i = 0; i < msgSize; ++i) {
+            String message = messageContentType == 0 ? raw : randomMessage;
+            sb.append(message);
+        }
+        String all = sb.toString();
+        System.out.printf("chunk打开：%d 压缩打开：%d 大小：%d",chunk,compre,msgSize);
+        System.out.println("消息大小为:"+all.length()/1024/1024+"MB");
+        System.out.println("压缩算法:"+zipTypeMap.get(zipType).toString());
+        for(int i = 0; i < msgCount; ++i) {
+            TimeUnit.MILLISECONDS.sleep(sleepms);
+            Long start = System.currentTimeMillis();
+            TypedMessageBuilder<byte[]> value = producer.newMessage().value(all.getBytes());
+            Long end = System.currentTimeMillis();
+            System.out.println("========计算压缩消息耗时===========");
+            System.out.println("消息压缩耗时:"+(end - start)+"毫秒");
+            System.out.println("========压缩消息耗时计算结束===========");
+            System.out.printf("发送第%d条： msgId: %s sequenceId %d\n",i+1,value.send(),i/10);
+        }
+
+        System.out.printf("生产完成 %d  producerName:%s \n",msgCount,producer.getProducerName());
+    }
+}
